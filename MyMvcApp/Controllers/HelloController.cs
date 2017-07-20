8b4a962ea4a84307f9dbb73435433cfa0bca8418
skmohtasim <skmohtasim@yahoo.com>
2017-07-20 12:41:25 +0600

    first commit

---
+++ MyMvcApp/Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMvcApp.Controllers
+{
+    public class HelloController : Controller
+    {
+        //
+        // GET: /Hello/
+
+        public ActionResult Mytemplate()
+        {
+            Response.Write("aaa");
+            return View();
+        }
+
+    }
+}
